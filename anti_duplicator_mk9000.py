a627b5bbb387fd9b07637757d65fe4323f51eb32
Bhargav <bhargavkadali39@gmail.com>
2021-09-21 12:44:38 +0530

    Update anti_duplicator_mk9000.py

--- anti_duplicator_mk9000.py
+++ anti_duplicator_mk9000.py
@@ -11,9 +11,14 @@ for i in List_1:
     
 print(List_2)
 
+# Still this old method is faster than the other.
+# Execution time is: 0.008489199999999975
+# That much doesn't matter much,not in the case while working with big amount of data.
 '''
 
 # Removing Duplicates using set() and List() methods.
 
 List_3 = list(set(List_1))
 print(List_3)
+
+# Execution time is: 0.011315100000000022
